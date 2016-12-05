5302745f63fab48b640f130e9e96cc8759ac1274
Erik Weitnauer <eweitnauer@gmail.com>
2016-12-05 11:07:48 -0500

    new exp. location 2.0

--- copy-to-server.sh
+++ copy-to-server.sh
@@ -1,2 +1,2 @@
 rsync -avz --exclude '.DS_Store' --exclude '.gitignore' --exclude '.git' --exclude 'copy-to-server.sh' --exclude 'README.md' \
-  -e "ssh" --delete ./* erik@graspablemath.com:/srv/www/graspablemath.com/public_html/studies/gm-gestures/1.0
+  -e "ssh" --delete ./* erik@graspablemath.com:/srv/www/graspablemath.com/public_html/studies/gm-gestures/2.0
